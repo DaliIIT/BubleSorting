c8a3406da9b10125216178e422c4221ac8901ecd
EUGENE <bagaeveugene@gmail.com>
2018-02-07 16:45:01 +0500

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,40 +7,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] Arr = new int[]{2, 1, 3};
+        int[] Arr = new int[]{2, 1, 3, 23, 12, 56, 1, 4, 8, 9, 11, 10, 4};
+        int i,j,k;
 
-        for (int k = 0; k < Arr.length; k++) {
-            System.out.println("Number: " + k + " value:" + Arr[k]);
+        System.out.println("Unsorted elements:");
+
+        for (k = 0; k < Arr.length; k++) {
+            System.out.println("№: " + k + " value:" + Arr[k]);
         }
 
 
-        for (int i = 0; i < Arr.length; i++) {
+            for (i = 0; i < Arr.length; i++) {
 
-            for (int j = 0; j < Arr.length - i; j++) {
-
-
-                if (Arr[j] > Arr[j + 1]) {
-                    int Temp = Arr[j + 1];
-                    Arr[j + 1] = Arr[j];
-                    Arr[j] = Temp;
-
-                } else {
+                for (j = 0; j < Arr.length - i -1; j++) {
 
 
+                    if (Arr[j] > Arr[j + 1]) {
+                        int Temp = Arr[j + 1];
+                        Arr[j + 1] = Arr[j];
+                        Arr[j] = Temp;
+
+                    } else {
+
+
+                    }
+                    ;
                 }
-
             }
 
-        }
+        System.out.println("Sorted elements:");
 
 
-//        for (int k: Arr) {
-//            System.out.println(Arr[k]);
-//        }
-
-
-        for (int k = 0; k < Arr.length; k++) {
-            System.out.println("Number: " + k + " value:" + Arr[k]);
+        for (k = 0; k < Arr.length; k++) {
+            System.out.println("№: " + k + " value:" + Arr[k]);
         }
 
 
